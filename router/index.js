966e43d38accff0bae5602009c4ab4ae6d088a37
Muhammad Owais <m.owais1045@gmail.com>
2024-10-05 19:20:15 +0500

    code upated

--- router/index.js
+++ router/index.js
@@ -2,6 +2,8 @@ module.exports = function (express, app) {
   app.use("/api/auth", require("./auth")(express, app));
   app.use("/api/football", require("./score")(express, app));
   app.use("/api/flash", require("./flashScore")(express, app));
+  app.use("/api/news", require("./news")(express, app));
+
   app.get("/", (req, res) => res.send({
     message: "Welcome to the Football API",
     version: "19.8.2024",
---
+++ router/news.js
@@ -0,0 +1,21 @@
+const newsController = require("../controller/news/index");
+
+module.exports = function (express, app, lib, db) {
+    const router = express.Router();
+
+    router.get("/", async (req, res) => {
+        try {
+            const news = await newsController(lib, db).fetchNews(req.query);
+            res.json({
+                data: news,
+                status: 200,
+                success: true,
+            });
+        } catch (error) {
+            // console.log("🚀 ~ router.get ~ error:", error)
+            res.status(500).json({ error: error.message, success: false });
+        }
+    });
+
+    return router;
+}
