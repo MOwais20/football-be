97bd827932d28c2ca1a1d49c9ced6984fa76b8d6
Muhammad Owais <m.owais1045@gmail.com>
2024-10-06 22:08:07 +0500

    code updatde

--- controller/news/index.js
+++ controller/news/index.js
@@ -1,66 +1,68 @@
 const axios = require("axios");
 module.exports = function (lib, db) {
-    const translate = async (obj) => {
-        if (!obj.content) {
-            return;
+  const translate = async (obj) => {
+    if (!obj.content) {
+      return;
+    }
+
+    let result = await axios
+      .post("http://localhost:5000/translate", {
+        q: obj.content,
+        source: "en",
+        target: obj.targetLanguage,
+      })
+      .then((response) => response.data)
+      .catch((error) => {
+        // console.log("🚀 ~ translate ~ error", error?.response?.data?.error);
+        throw new Error(error?.message || "Error translating the content");
+      });
+
+    return result?.translatedText;
+  };
+
+  const fetchNews = async (params) => {
+    try {
+      const news = await axios
+        .get("https://apis.soccernetnews.com/crypto/news?v=v1")
+        .then((response) => response?.data)
+        .catch((error) => {
+          throw new Error(error?.message || "Error fetching news");
+        });
+
+      if (params.targetLanguage === "en") {
+        return news.data;
+      } else {
+        let translatedNews = [];
+        // create a for loop to iterate over the news array
+        for (let i = 0; i < news.data.length; i++) {
+          if (i == 10) break;
+
+          news.data[i].title = await translate({
+            content: news.data[i].title,
+            targetLanguage: params.targetLanguage,
+          });
+
+          news.data[i].createtime = await translate({
+            content: news.data[i].createtime,
+            targetLanguage: params.targetLanguage,
+          });
+
+          news.data[i].content = await translate({
+            content: news.data[i].content,
+            targetLanguage: params.targetLanguage,
+          });
+
+          translatedNews.push(news.data[i]);
         }
 
-        let result = await axios
-            .post("http://localhost:5000/translate", {
-                q: obj.content,
-                source: "en",
-                target: obj.targetLanguage,
-            })
-            .then((response) => response.data)
-            .catch((error) => {
-                // console.log("🚀 ~ translate ~ error", error?.response?.data?.error);
-                throw new Error(error?.message || "Error translating the content");
-            });
+        return translatedNews;
+      }
+    } catch (error) {
+      throw new Error(error.message);
+    }
+  };
 
-        return result?.translatedText;
-    };
-
-    const fetchNews = async (params) => {
-        try {
-            const news = await axios
-                .get("https://apis.soccernetnews.com/crypto/news?v=v1")
-                .then((response) => response?.data)
-                .catch((error) => {
-                    throw new Error(error?.message || "Error fetching news");
-                });
-
-
-            if (params.targetLanguage === "en") {
-                return news.data;
-            } else {
-                // create a for loop to iterate over the news array
-                for (let i = 0; i < news.data.length; i++) {
-                    news.data[i].title = await translate({
-                        content: news.data[i].title,
-                        targetLanguage: params.targetLanguage,
-                    });
-
-                    news.data[i].createtime = await translate({
-                        content: news.data[i].createtime,
-                        targetLanguage: params.targetLanguage,
-                    });
-
-                    news.data[i].content = await translate({
-                        content: news.data[i].content,
-                        targetLanguage: params.targetLanguage,
-                    });
-                }
-
-                return news.data;
-            }
-
-
-        } catch (error) {
-            throw new Error(error.message);
-        }
-    };
-
-    return {
-        fetchNews,
-    };
+  return {
+    fetchNews,
+  };
 };
