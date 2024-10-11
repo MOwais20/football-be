3e03670fdc4ded290a0b78675c0e6ef433ac1fdd
Muhammad Owais <m.owais1045@gmail.com>
2024-10-11 16:37:50 +0500

    symbols api created

--- router/news.js
+++ router/news.js
@@ -17,5 +17,19 @@ module.exports = function (express, app, lib, db) {
         }
     });
 
+    router.get("/symbols", async (req, res) => {
+        try {
+            const news = await newsController(lib, db).fetchSymbols(req.query);
+            res.json({
+                data: news,
+                status: 200,
+                success: true,
+            });
+        } catch (error) {
+            // console.log("🚀 ~ router.get ~ error:", error)
+            res.status(500).json({ error: error.message, success: false });
+        }
+    });
+
     return router;
 }
