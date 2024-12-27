5deb441b4fd78f9e5e2fe2e71b1720c3d5ca1e39
Muhammad Owais <m.owais1045@gmail.com>
2024-12-28 02:49:33 +0500

    filteroud code

--- config/database.js
+++ config/database.js
@@ -2,8 +2,7 @@ const { MongoClient } = require("mongodb");
 require("dotenv").config();
 
 const uri = process.env.MONGO_URI;
-const client = new MongoClient(uri);
-
+const client = new MongoClient(uri || "mongodb://localhost:27017");
 const DB = client.db("football_score_app");
 
 module.exports = DB;
