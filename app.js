b719d927cbd51acd27694c656b974bbd6c3b463c
Muhammad Owais <m.owais1045@gmail.com>
2024-12-27 17:56:58 +0500

    code updated

--- app.js
+++ app.js
@@ -17,6 +17,8 @@ app.use(express.json());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res) => {
+  console.log("Hello");
+  
   res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
