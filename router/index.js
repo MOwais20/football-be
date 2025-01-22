f1947cb90d296dee8860428e55ea9e487184c298
Muhammad Owais <m.owais1045@gmail.com>
2025-01-22 21:49:58 +0500

    code updated

--- router/index.js
+++ router/index.js
@@ -1,7 +1,7 @@
 
 
 module.exports = function (express, app) {
-  app.use("/api/auth", require("./auth")(express, app));
+  // app.use("/api/auth", require("./auth")(express, app));
   app.use("/api/news", require("./news")(express, app));
   // app.get("/", (req, res) => res.send({
   //   message: "Welcome to the Football API",
